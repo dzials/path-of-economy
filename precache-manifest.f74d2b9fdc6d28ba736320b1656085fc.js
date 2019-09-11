bd857bb122900e6a4b66b31a3b79bc51f94817fa
dzials <stephendzialo7@gmail.com>
2019-09-11 00:52:31 -0400

    Updates

---
+++ precache-manifest.f74d2b9fdc6d28ba736320b1656085fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc0a6bae0f947dd287b44d4050e01a9",
+    "url": "/path-of-economy/index.html"
+  },
+  {
+    "revision": "980042d398e8ffbe8f05",
+    "url": "/path-of-economy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ea7bc7b594bf80ba75c2",
+    "url": "/path-of-economy/static/js/2.c8a37398.chunk.js"
+  },
+  {
+    "revision": "980042d398e8ffbe8f05",
+    "url": "/path-of-economy/static/js/main.223e07af.chunk.js"
+  },
+  {
+    "revision": "0600e39c396a686a3f75",
+    "url": "/path-of-economy/static/js/runtime~main.a781058b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/path-of-economy/static/media/logo.5d5d9eef.svg"
+  }
+]);
